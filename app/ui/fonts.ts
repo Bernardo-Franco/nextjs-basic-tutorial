dab266a18ee8ee526fa09f00a4c95b6f106b9771
Bernardo-Franco <bernard.f651@gmail.com>
2024-03-14 06:54:57 -0500

    first commit learning nextjs with the nextjs tutorial -todo-> about to setup the database

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Rubik, Lusitana } from 'next/font/google';
+
+export const inter = Rubik({ weight: '400', subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
